9ea2ceae441d5330a967a7270a9fde60cbb63101
Rafael Pazos <rarod@microsoft.com>
2021-08-17 12:01:02 +0200

    fix output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,3 @@
-output "application_hostname" {
-  value       = module.application.application_hostname
-  description = "The Web application URL."
-}
-
 output "resource_group" {
   value       = local.resource_group
   description = "The resource group."
